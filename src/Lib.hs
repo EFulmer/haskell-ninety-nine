21e844d3e78bde63c8edf96fceb31d235b302140
Eric Fulmer <EFulmer@gmail.com>
2016-04-06 08:18:25 -0400

    problems two and three

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( someFunc
     , problemOne
+    , problemTwo
+    , problemThree
     ) where
 
 someFunc :: IO ()
@@ -10,3 +12,14 @@ problemOne :: [a] -> Maybe a
 problemOne []     = Nothing
 problemOne (x:[]) = Just x
 problemOne (x:xs) = problemOne xs
+
+problemTwo :: [a] -> Maybe a
+problemTwo []       = Nothing
+problemTwo [x]      = Nothing
+problemTwo (x:y:[]) = Just x
+problemTwo (x:xs)   = problemTwo xs
+
+problemThree :: [a] -> Int -> Maybe a
+problemThree [] k     = Nothing
+problemThree (x:xs) 1 = Just x
+problemThree (x:xs) k = problemThree xs (pred k)
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,7 @@ module Spec
   main
   ) where
 
-import Safe (lastMay)
+import Safe            (atMay, initMay, lastMay)
 
 import Test.QuickCheck
 
@@ -11,6 +11,12 @@ import Lib
 
 prop_ProblemOne :: (Eq a) => [a] -> Bool
 prop_ProblemOne xs = problemOne xs == lastMay xs
+
+prop_ProblemTwo :: (Eq a) => [a] -> Bool
+prop_ProblemTwo xs = problemTwo xs == (initMay xs >>= lastMay)
+
+prop_ProblemThree :: (Eq a) => [a] -> Int -> Bool
+prop_PoblemThree xs k = (problemThree xs k) == (atMay xs (pred k))
 
 main :: IO ()
 main = quickCheck $ \xs -> prop_ProblemOne (xs :: [Int])
